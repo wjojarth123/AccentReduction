448cd5910bdcddb0f895be5bdb4456c4333322a0
William Jojarth <williamjojarth@Williams-MBP.hsd1.ca.comcast.net>
2020-08-07 09:40:30 -0700

    basic code idea

--- Accentreduction.py
+++ Accentreduction.py
@@ -1,2 +1,22 @@
 import speech
-speech.say('Hola mundo', 'es_ES')
+import sound
+from random_word import RandomWords
+r = RandomWords()
+playing=True
+words=0
+inaccuracy=0
+for i in range(10):
+    words+=1
+    Word=True
+    word=r.get_random_word()
+    while Word:
+        speech.say(word, 'es_ES')
+        r = sound.Recorder('audio.m4a')
+        r.record(3)
+        text = speech.recognize('audio.m4a', 'en')[0][0]
+        if text==word:
+            Word=False
+        else:
+            inaccuracy+=1
+            print("try again")
+print(inaccuracy/words)
